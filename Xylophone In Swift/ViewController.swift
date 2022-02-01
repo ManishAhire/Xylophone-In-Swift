85c1aac3354b0e62084562fb2cc50dc32fc4da7c
Manish Ahire <ahiremanishb@gmail.com>
2022-02-01 14:07:43 +0530

    Initial Commit

--- Xylophone In Swift/ViewController.swift
+++ Xylophone In Swift/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let keyName = sender.titleLabel?.text else {
+            print("No title for button")
+            return
+        }
+        sender.layer.opacity = 0.5
+        playSound(for: keyName)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            sender.layer.opacity = 1.0
+        }
+    }
+    
+    func playSound(for keyName : String) {
+        
+        guard let soundPath = Bundle.main.url(forResource: keyName, withExtension: ".wav") else {
+            print("Sound file not found")
+            return
+        }
+        player = try! AVAudioPlayer(contentsOf: soundPath)
+        player.play()
+    }
 }
 
